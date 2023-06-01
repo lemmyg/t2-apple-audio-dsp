dcd61c6c3e8abeb7c767befda6eeb60479061aac
galder <lemmyg@gmail.com>
2023-06-01 23:44:34 +0100

    improve python script

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 # SPDX-License-Identifier: MIT
 # (C) 2023 Linux T2 Kernel Team
 echo "Install DSP config as root ..."
-sudo sh -c 'python3 main.py'
+#cmd = 'python3 main.py "$@"'
+sudo bash -c 'python3 main.py "$@"' -- "$@"
 echo "Restarting Pipewire for current user ...."
 systemctl --user restart wireplumber pipewire pipewire-pulse
 echo "Note that the first time you may need to restart your computer."
