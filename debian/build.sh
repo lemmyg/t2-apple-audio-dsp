645403547862140c7365ff0d0f4e03f97e2126fd
galder <lemmyg@gmail.com>
2023-06-05 17:43:38 +0100

    improved debian generation

--- debian/build.sh
+++ debian/build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 mkdir temp
 mkdir temp/DEBIAN
-cp debian/control temp/DEBIAN/control
+control=debian/control
+control=debian/control
+# get version from control file
+version=$(grep "Version:" "$control" | cut -d ' ' -f 2)
+package=$(grep "Package:" "$control" | cut -d ' ' -f 2)
+#copy files
+cp $control temp/DEBIAN/control
 
 mkdir -p temp/etc/pipewire/pipewire.conf.d
-cp  config/10-t2_161_speakers.conf temp/etc/pipewire/pipewire.conf.d
+cp config/10-t2_161_speakers.conf temp/etc/pipewire/pipewire.conf.d
 
 mkdir -p temp/usr/share/pipewire/devices/apple
 cp firs/*.wav temp/usr/share/pipewire/devices/apple
 
-dpkg-deb --build --root-owner-group -Zgzip temp
-mv temp.deb t2-apple-audio-dsp-speakers161_1.0.0_amd64.deb
+#generate debian package
+output="${package}_${version}_amd64.deb"
+echo "package ${output}"
+dpkg-deb --root-owner-group -Zgzip --build temp $output
+echo "tag=$(ls t2-apple-audio-dsp-*)"
