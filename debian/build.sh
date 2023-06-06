6d1773e4f5d901817965ea60e0723d55bba652f1
galder <lemmyg@gmail.com>
2023-06-06 22:56:30 +0100

    added debian postinst

--- debian/build.sh
+++ debian/build.sh
@@ -2,12 +2,12 @@
 mkdir temp
 mkdir temp/DEBIAN
 control=debian/control
-control=debian/control
 # get version from control file
 version=$(grep "Version:" "$control" | cut -d ' ' -f 2)
 package=$(grep "Package:" "$control" | cut -d ' ' -f 2)
 #copy files
 cp $control temp/DEBIAN/control
+cp debian/postinst temp/DEBIAN/postinst
 
 mkdir -p temp/etc/pipewire/pipewire.conf.d
 cp config/10-t2_161_speakers.conf temp/etc/pipewire/pipewire.conf.d
@@ -20,3 +20,6 @@ output="${package}_${version}_amd64.deb"
 echo "package ${output}"
 dpkg-deb --root-owner-group -Zgzip --build temp $output
 echo "tag=$(ls t2-apple-audio-dsp-*)"
+# remove temp folder
+rm -r temp
+
