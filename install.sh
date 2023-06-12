0129f9707d2cb2ffde2cff357fdfec13f19ca8e1
galder <lemmyg@gmail.com>
2023-06-12 19:31:52 +0100

    fixed folder creation bug

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 # (C) 2023 Linux T2 Kernel Team
 echo "Install Macbook Pro 16.1 speakers DSP config as root"
 echo "Copying 10-t2_161_speakers.conf to /etc/pipewire/pipewire.conf.d"
+sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo cp  config/10-t2_161_speakers.conf /etc/pipewire/pipewire.conf.d
 echo "Copying firs/*.wav to /usr/share/pipewire/devices/apple"
+sudo mkdir -p /usr/share/pipewire/devices/apple
 sudo cp firs/*.wav /usr/share/pipewire/devices/apple
 echo "Restarting Pipewire for current user ...."
 systemctl --user restart wireplumber pipewire pipewire-pulse
