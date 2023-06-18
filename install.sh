ab3f3d35ea65508cf980c92e541e7886fa7adac9
galder <lemmyg@gmail.com>
2023-06-18 10:51:50 +0100

    added headset install code

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ echo "Install Mic DSP config as root"
 echo "Copying 10-t2_mic.conf to /etc/pipewire/pipewire.conf.d"
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo cp  config/10-t2_mic.conf /etc/pipewire/pipewire.conf.d/10-t2_mic.conf
+sudo cp config/10-t2_headset_mic.conf /etc/pipewire/pipewire.conf.d/10-t2_mic.conf
+
 echo "Restarting Pipewire for current user ...."
 systemctl --user restart wireplumber pipewire pipewire-pulse
 echo "Note that the first time you may need to restart your computer."
