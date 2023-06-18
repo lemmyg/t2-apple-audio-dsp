8bc1859ddf4bbc8ed9efaf2132fb3c5a466e1890
galder <lemmyg@gmail.com>
2023-06-18 11:25:45 +0100

    fixed overwriting mic config

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Install Mic DSP config as root"
 echo "Copying 10-t2_mic.conf to /etc/pipewire/pipewire.conf.d"
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo cp  config/10-t2_mic.conf /etc/pipewire/pipewire.conf.d/10-t2_mic.conf
-sudo cp config/10-t2_headset_mic.conf /etc/pipewire/pipewire.conf.d/10-t2_mic.conf
+sudo cp config/10-t2_headset_mic.conf /etc/pipewire/pipewire.conf.d/10-t2_headset_mic.conf
 
 echo "Restarting Pipewire for current user ...."
 systemctl --user restart wireplumber pipewire pipewire-pulse
