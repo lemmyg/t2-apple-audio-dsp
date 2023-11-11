9422c57caeb54fde45121b9ea31628080da9d3bd
Galder <lemmyg@gmail.com>
2023-11-11 20:57:27 +0000

    Removed mixer plugin and capturing only from AUX1.

---
+++ CHANGES.md
@@ -0,0 +1,7 @@
+CHANGES
+-------
+
+* 0.4.0-1:
+    * 10-t2_mic: Removed mixer plugin and capturing only from AUX1.
+    * Removed "Amplitude (dB)" output parameters as not need it.
+    * Uninstall.sh: Added missing path.
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@
 # (C) 2023 Linux T2 Kernel Team
 echo "Uninstall Mic DSP config as root"
 sudo rm /etc/pipewire/pipewire.conf.d/10-t2_mic.conf
+sudo rm /etc/pipewire/pipewire.conf.d/10-t2_headset_mic.conf
 echo "Restarting Pipewire for current user ...."
 systemctl --user restart wireplumber pipewire pipewire-pulse
 echo "Note that the first time you may need to restart your computer."
