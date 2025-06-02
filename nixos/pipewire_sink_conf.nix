cc3cd0a7d80561a315c39e6175f416070cdae98d
zinger <zinger@zingerkot.home.arpa>
2025-06-02 16:28:55 +0300

    Updated pipewire_sink_conf.nix to fetch speakers-v0.2.0-1 and NixOS 25.05

--- nixos/pipewire_sink_conf.nix
+++ nixos/pipewire_sink_conf.nix
@@ -7,20 +7,25 @@ let
   t2AppleAudioDSP = pkgs.fetchFromGitHub {
     owner = "lemmyg";
     repo = "t2-apple-audio-dsp";
-    rev = "fdd10303c8e84d09cf31e8cd88a624a34b9ed186";
-    sha256 = "UnoZMONaYyhXkntTN+iUwcyjSquIgcw/Tw+ooNX9CzA=";
+    rev = "971833705c746b1b02e5a844c4ca7e2d86582dc9";
+    sha256 = "iPS2wVNdH02NpDsymtMIqWlDMPXLdIBTdbH4uYe9OIE=";
   };
   originalTweeterFilePath = "/usr/share/pipewire/devices/apple/macbook_pro_t2_16_1_tweeters-48k_4.wav";
   newTweeterFilePath = "${t2AppleAudioDSP}/firs/macbook_pro_t2_16_1_tweeters-48k_4.wav";
   originalWooferFilePath = "/usr/share/pipewire/devices/apple/macbook_pro_t2_16_1_woofers-48k_4.wav";
   newWooferFilePath = "${t2AppleAudioDSP}/firs/macbook_pro_t2_16_1_woofers-48k_4.wav";
   configFile = "${t2AppleAudioDSP}/config/10-t2_161_speakers.conf";
+
+  dspSinkConfig = pkgs.runCommand 
+    "10-t2_161_speakers.conf"
+    { }
+    ''
+    cat ${configFile} | sed -e 's|${originalTweeterFilePath}|${newTweeterFilePath}|g' \
+      -e 's|${originalWooferFilePath}|${newWooferFilePath}|g' > $out
+    '';
 in
 {
-  environment.etc."pipewire/pipewire.conf.d/10-t2_161-sink.conf".source = pkgs.runCommand
-    "10-t2_161_speakers.conf"
-    { } ''
-    sed -e 's|${originalTweeterFilePath}|${newTweeterFilePath}|g' \
-        -e 's|${originalWooferFilePath}|${newWooferFilePath}|g' ${configFile} > $out
-  '';
+  services.pipewire.configPackages = [
+    (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-t2_161-sink.conf" (builtins.readFile "${dspSinkConfig}"))
+  ];
 }
