340024cde1d8bb6277df8da479a1d923009656ea
Andrew Emerick <johndoe@example.com>
2025-10-14 13:39:00 -0400

    Add macbook air config files

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 # (C) 2023 Linux T2 Kernel Team
-echo "Install Macbook Pro 16.1 speakers DSP config as root"
-echo "Copying 10-t2_161_speakers.conf to /etc/pipewire/pipewire.conf.d"
+echo "Install Macbook Air 9,1 speakers DSP config as root"
+echo "Copying t2_91_speakers.conf to /etc/pipewire/pipewire.conf.d"
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
-sudo cp  config/10-t2_161_speakers.conf /etc/pipewire/pipewire.conf.d
-echo "Copying firs/*.wav to /usr/share/pipewire/devices/apple"
-sudo mkdir -p /usr/share/pipewire/devices/apple
-sudo cp firs/*.wav /usr/share/pipewire/devices/apple
+sudo cp  config/t2_91_speakers.conf /etc/pipewire/pipewire.conf.d
+echo "Copying firs/*.wav to /usr/share/pipewire/devices/apple/air91"
+sudo mkdir -p /usr/share/pipewire/devices/apple/air91
+sudo cp firs/*.wav /usr/share/pipewire/devices/apple/air91
 echo "Restarting Pipewire for current user ...."
 systemctl --user restart wireplumber pipewire pipewire-pulse
 echo "Note that the first time you may need to restart your computer."
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 # (C) 2023 Linux T2 Kernel Team
-echo "Uninstall Macbook Pro 16.1 speakers DSP config as root"
-sudo rm /etc/pipewire/pipewire.conf.d/10-t2_161_speakers.conf
-sudo rm /usr/share/pipewire/devices/apple/macbook_pro_t2_16_1_*.wav
+echo "Uninstall Macbook Air 9,1 speakers DSP config as root"
+sudo rm /etc/pipewire/pipewire.conf.d/t2_91_speakers.conf
+sudo rm /usr/share/pipewire/devices/apple/air91/*.wav
 echo "Restarting Pipewire for current user ...."
 systemctl --user restart wireplumber pipewire pipewire-pulse
 echo "Note that the first time you may need to restart your computer."
